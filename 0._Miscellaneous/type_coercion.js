52d4244569ba10a5a35caf14eebd763b55b94286
Anders Latif <andl@kea.dk>
2022-08-29 09:55:44 +0200

    Added the type coercion file and intro exercises basics file

---
+++ 0._Miscellaneous/type_coercion.js
@@ -0,0 +1,9 @@
+// type coersion lecture
+const total = 34.32;
+const price = 12.32;
+
+const newTotal = total + price;
+console.log(newTotal);
+
+const balanceEquals = total === newTotal;
+console.log(balanceEquals);
