8fd16c4eba4f4d78606dc079ddf9a411f3b7793a
Anders Latif <andl@kea.dk>
2022-08-29 10:33:36 +0200

    Solved 0._Basics in class

--- 0._Miscellaneous/intro_exercises/0._Basics.js
+++ 0._Miscellaneous/intro_exercises/0._Basics.js
@@ -10,6 +10,19 @@ const lastName = "Latif";
 // Print out the following in the console:
 // My first name is Anders and my last name is Latif
 
+// const message = "My first name is " + firstName + " and my last name is " + lastName;
+// console.log(message);
+
+// console.log("My first name is", firstName, "and my last name is", lastName);
+
+// const message = `My first name is ${firstName} and my last name is ${lastName}`;
+// console.log(message);
+
+
+const messageVersionOne = "It's so easy.";
+const messageVersionTwo = 'I can write " easily.';
+const messageVersionThree = `'''"""''''"""`;
+
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -21,7 +34,16 @@ const number = 1;
 // The result should be 2022
 // You cannot touch line 1 or 2
 
+// const result = parseInt(year) + number;
+// const result = Number(year) + number;
 
+// const result = ~~year + number;
+// const result = +year + number;
+
+// console.log(result);
+
+console.log(parseInt("32432432fsdfdsf332432dsf"));
+console.log(Number("32432432fsdfdsf332432dsf"));
 
 // --------------------------------------
 
