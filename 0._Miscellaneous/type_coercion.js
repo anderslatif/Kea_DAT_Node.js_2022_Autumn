35db0fdbaa710765eab71bb9865a42523fc3b3d0
Anders Latif <andl@kea.dk>
2022-08-29 10:40:12 +0200

    Added the rest of the basics files

--- 0._Miscellaneous/type_coercion.js
+++ 0._Miscellaneous/type_coercion.js
@@ -7,3 +7,15 @@ console.log(newTotal);
 
 const balanceEquals = total === newTotal;
 console.log(balanceEquals);
+
+
+const cat = {
+    key: "value", 
+    name: "Tom"
+};
+
+// square-bracket notation
+console.log(cat["name"]);
+// dot notation
+console.log(cat.name);
+
