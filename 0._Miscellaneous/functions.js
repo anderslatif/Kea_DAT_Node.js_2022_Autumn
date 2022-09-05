c40d97a3cc988bd05387aecf06124b19ac1102f4
Anders Latif <andl@kea.dk>
2022-09-05 09:49:42 +0200

    Finished the first exercise with callback functions

--- 0._Miscellaneous/functions.js
+++ 0._Miscellaneous/functions.js
@@ -18,7 +18,12 @@ const addArrowFunctionCompact = (a, b) => a + b;
 
 function genericActionExecutor(anyCallbackFunction, name) {
     // execute some code ...
-    anyCallbackFunction(name);
+    return anyCallbackFunction(name);
 }
 
+const spitting = (name) => `${name} is spitting.`;
 
+// task Write a single line below without changing the above.
+// task the output to the terminal should be: Amanda is spitting. 
+// task don't call spitting directly.. use spitting as a callback function
+console.log(genericActionExecutor(spitting, "Amanda"));
