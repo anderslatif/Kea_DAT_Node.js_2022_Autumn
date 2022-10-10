8208fdc326e1202303c501458262350005733824
Anders Latif <andl@kea.dk>
2022-10-10 11:46:39 +0200

    Created the Date component and showed how reactivity works

--- 5._First_Svelte_Project/src/App.svelte
+++ 5._First_Svelte_Project/src/App.svelte
@@ -1,12 +1,15 @@
 <script>
+  import Date from "./components/Date.svelte";
+
   const name = "Anders";
-  const currentDate = new Date();
+
 </script>
 
 
 <h2 id="page-title">First Svelte Project</h2>
 <div>More content for {name}.</div>
-<div>Curent Date: {currentDate}</div>
+<Date></Date>
+
 
 <style>
   #page-title {
