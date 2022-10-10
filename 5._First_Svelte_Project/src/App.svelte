0867e700184d8989b18ec95143212c67840cce20
Anders Latif <andl@kea.dk>
2022-10-10 11:29:27 +0200

    Created the first Svelte project

---
+++ 5._First_Svelte_Project/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  const name = "Anders";
+  const currentDate = new Date();
+</script>
+
+
+<h2 id="page-title">First Svelte Project</h2>
+<div>More content for {name}.</div>
+<div>Curent Date: {currentDate}</div>
+
+<style>
+  #page-title {
+    color: blue;
+  }
+</style>
