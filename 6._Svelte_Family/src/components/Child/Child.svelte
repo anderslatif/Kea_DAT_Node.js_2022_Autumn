50d7adf4361fb29e4baa2354357b5c26627c0db3
Anders Latif <andl@kea.dk>
2022-10-31 09:22:48 +0100

    Sent data from Child to Parent through function references

--- 6._Svelte_Family/src/components/Child/Child.svelte
+++ 6._Svelte_Family/src/components/Child/Child.svelte
@@ -1,5 +1,10 @@
 <script>
     export let child;
+    export let parentName;
+    export let onAlwaysSayHi;
+    export let onTellILoveYou;
 </script>
 
 <p>{child.name}</p>
+<button on:click={onAlwaysSayHi}>Greet</button>
+<button on:click={onTellILoveYou(`I love you, ${parentName}.`)}>Whisper in parents ear</button>
--- 6._Svelte_Family/src/components/Parent/Parent.svelte
+++ 6._Svelte_Family/src/components/Parent/Parent.svelte
@@ -4,9 +4,17 @@
 
     import Child from "../Child/Child.svelte";
 
+    function handleAlwaysSayHi() {
+        console.log("Hi");
+    }
+
+    function handleTellILoveYou(message) {
+        console.log(message);
+    }
+
 </script>
 
 <h1>Hi I'm {name}.</h1>
 {#each children as child}
-    <Child child={child} />
+    <Child child={child} parentName={name} onAlwaysSayHi={handleAlwaysSayHi} onTellILoveYou={handleTellILoveYou} />
 {/each}
