473c6d681b39a1c3077e2ccb82f08aa48c191dc7
Anders Latif <andl@kea.dk>
2022-11-14 11:38:10 +0100

    Implemented get and post for the animalsrouter

--- 8._zoo/server/app.js
+++ 8._zoo/server/app.js
@@ -4,14 +4,13 @@ const app = express();
 // import path from "path";
 // app.use(express.static(path.resolve("../client/dist")));
 
+app.use(express.json());
+
 import cors from "cors";
 app.use(cors());
 
-const animals = ["🦩", "🐶", "🦍", "🦄", "🦄", "🦚"];
-
-app.get("/api/animals", (req, res) => {
-    res.send({ data: animals });
-});
+import animalsRouter from "./routers/animalsRouter.js";
+app.use(animalsRouter);
 
 const PORT = 8080 || process.env.PORT;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
