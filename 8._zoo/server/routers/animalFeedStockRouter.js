d94e1bb3dfb5c4ad9dbfdeb43e2e9cc5d53ec56a
Anders Latif <andl@kea.dk>
2022-11-21 12:07:06 +0100

    Finished the POST animal feed stock route

--- 8._zoo/server/routers/animalFeedStockRouter.js
+++ 8._zoo/server/routers/animalFeedStockRouter.js
@@ -8,9 +8,16 @@ router.get("/animalfeedstock", async (req, res) => {
     res.send({ data: rows });
 });
 
-router.post("/animalfeedstock/:stockTypeId", (req, res) => {
-    // task write pseudo-code or comments and reflect on what steps you need to do here
-    res.send({});
+router.post("/animalfeedstock", async (req, res) => {
+    const { initialAmount, remainingAmount, animalFeedTypeId } = req.body;
+
+    if (!animalFeedTypeId) return res.status(400).send({ message: "Missing the key animalFeedTypeId" });
+    if (!initialAmount) return res.status(400).send({ message: "Missing the key initialAmount" });
+
+    const remainingAmountDefault = remainingAmount || initialAmount
+    const [result, _] = await db.execute("INSERT INTO animal_feed_stock (initial_amount, remaining_amount, animal_feed_type_id) VALUES (?, ?, ?);", [initialAmount, remainingAmountDefault, animalFeedTypeId]);
+
+    res.send({ affectedRows: result.affectedRows });
 });
 
 export default router;
