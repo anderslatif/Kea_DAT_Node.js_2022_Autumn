40817b5439e8854d6b7803cf348caa7c39daf5dc
Anders Latif <andl@kea.dk>
2022-11-21 09:47:17 +0100

    Finished the DDL scaffold for MySQL

--- 8._zoo/server/database/ddl_mysql.js
+++ 8._zoo/server/database/ddl_mysql.js
@@ -21,8 +21,9 @@ db.execute(`CREATE TABLE IF NOT EXISTS animal_feed_types (
 
 db.execute(`CREATE TABLE IF NOT EXISTS animal_feed_stock (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    comment VARCHAR(255),
-    addded_to_stock TIMESTAMP,
+    initial_amount INTEGER,
+    remaining_amount INTEGER,
+    addded_to_stock TIMESTAMP NOT NULL DEFAULT NOW(),
     animal_feed_type_id INTEGER,
     CONSTRAINT fk_feed_type FOREIGN KEY (id) REFERENCES animal_feed_types(id)
 );
@@ -30,11 +31,13 @@ db.execute(`CREATE TABLE IF NOT EXISTS animal_feed_stock (
 
 // seed the database
 if (isInDeleteMode) {
-    // db.execute(`INSERT INTO animal_feed_types (????) VALUES (???????);`);
+    db.execute(`INSERT INTO animal_feed_types (name) VALUES ('carrot');`);
+    db.execute(`INSERT INTO animal_feed_types (name) VALUES ('zebra');`);
+    db.execute(`INSERT INTO animal_feed_types (name) VALUES ('mosquitoes');`);
 
 
-    // db.execute(`INSERT INTO animal_feed_stock (????) VALUES (???????);`);
-
+    db.execute(`INSERT INTO animal_feed_stock (initial_amount, remaining_amount, animal_feed_type_id) VALUES (120, 120, 1);`);
+    db.execute(`INSERT INTO animal_feed_stock (initial_amount, remaining_amount, animal_feed_type_id) VALUES (2000, 2000, 3);`);
 }
 
 db.end();
