cec7fec15b1604173957ea1755585115aa025de6
Anders Latif <andl@kea.dk>
2022-12-05 10:11:00 +0100

    Finished sockets plain html example

--- 10._Sockets_Basic_HTML/app.js
+++ 10._Sockets_Basic_HTML/app.js
@@ -11,6 +11,17 @@ const io = new Server(server);
 io.on("connection", (socket) => {
     console.log(`A socket connected on id ${socket.id}`);
 
+    socket.on("client choose a color", (data) => {
+
+        // emits a broadcast to all sockets but itself
+        // socket.broadcast.emit("this is the new color", data);
+
+        // emits only to the socket itself
+        // socket.emit("this is the new color", data);
+
+        // emits to all sockets in the io namespace
+        // io.emit("this is the new color", data);
+    });
 
     io.on("disconnect", () => {
         console.log(`Socket ${socket.id} left.`);
